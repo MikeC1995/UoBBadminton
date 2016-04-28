fcd7ead83b63f7fa04e5e53f3dec18a551dd09cc
Mike Christensen <MikeC1995@users.noreply.github.com>
2016-04-28 11:45:32 +0100

    Update EDIT-ME.js

--- app/EDIT-ME.js
+++ app/EDIT-ME.js
@@ -4,6 +4,6 @@ var app = angular.module("app", []);
 app.controller("editController", function($scope) {
 
   // EDIT COMMITTEE NAMES HERE
-  $scope.committeeNames = ["Cameron", "Pablo", "Tom", "Pippa"];
+  $scope.committeeNames = ["John", "Pablo", "Tom", "Pippa", "Harry"];
 
 });
