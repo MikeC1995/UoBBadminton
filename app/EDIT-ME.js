d1454b31b9f45bc04943f4a3a8333eb70349a867
MikeC1995 <mike_christensen@btinternet.com>
2016-04-28 11:34:29 +0100

    demo

---
+++ app/EDIT-ME.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var app = angular.module("app", []);
+app.controller("editController", function($scope) {
+
+  // EDIT COMMITTEE NAMES HERE
+  $scope.committeeNames = ["Cameron", "Pablo", "Emma"];
+
+});
